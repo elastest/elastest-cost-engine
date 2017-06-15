bbbfa78bfa58784653f2a0b96f4f233f07f2a71b
Manuel Perez Belmonte <perz@zhaw.ch>
2017-06-15 10:51:11 +0200

    Jenkins tests commit

---
+++ src/main/java/io/elastest/ece/EceApplication.java
@@ -0,0 +1,8 @@
+package io.elastest.ece;
+
+public class EceApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
---
+++ src/test/java/elastest/io/ece/EceApplicationTests.java
@@ -0,0 +1,34 @@
+package elastest.io.ece;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class EceApplicationTests
+        extends TestCase {
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public EceApplicationTests(String testName) {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(EceApplicationTests.class);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp() {
+        assertTrue(true);
+    }
+}
